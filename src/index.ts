9bf251105f6f3a560ecb290adc70d2857b1be51c
tomokazukimizuka <tomokazukimizuka@gmail.com>
2023-12-14 15:59:20 +0900

    nuxt.config.tsのsessionキーにredisキーが存在する場合は保存先をRedisにする

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,33 @@ import type { SessionOptions } from 'express-session'
 import session from 'express-session'
 import { eventHandler, fromNodeMiddleware } from 'h3'
 import type { EventHandler, NodeMiddleware } from 'h3'
+import connectRedis from 'connect-redis'
+import { createClient } from 'redis'
 import type { Session } from './types'
 declare module 'h3' {
   interface H3EventContext {
     session: Session
   }
 }
+interface SessionOptionsRedis extends SessionOptions {
+  redis: {
+    ttl: number
+    url: string
+  }
+}
 
-export function createSessionHandler(options: SessionOptions): EventHandler[] {
+export function createSessionHandler(options: SessionOptionsRedis): EventHandler[] {
+  // Redisの設定がある場合のみoptionsに追加する
+  if (options && Object.prototype.hasOwnProperty.call(options, 'redis')) {
+    const redisClient = createClient({ url: options.redis.url ?? 'redis://localhost:6379' })
+    redisClient.connect().catch(console.error)
+    const RedisStore: connectRedis.RedisStore = connectRedis(session)
+    const redisStore = new RedisStore({
+      client: redisClient,
+      ttl: options.redis.ttl,
+    })
+    options.store = redisStore
+  }
   return [
     eventHandler((event) => {
       (event.node.res as any)._implicitHeader = () => {
